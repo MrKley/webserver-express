b1a5730fb290c91188a5d350f28a4d5a08023998
Kleyner <kvasconezc@unemi.edu.ec>
2020-11-23 21:20:46 -0500

    Primero Commit

---
+++ hbs/helpers.js
@@ -0,0 +1,10 @@
+const hbs = require('hbs');
+
+hbs.registerHelper('getAnio', () => new Date().getFullYear());
+hbs.registerHelper('capitalizar', (texto) => {
+    let palabras = texto.split(' ');
+    palabras.forEach((palabra, idx) => {
+        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    });
+    return palabras.join(' ');
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+//helpers
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'kleyner'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
